47d940b96a0cae248661593c4cf192308d0d1630
Nadea <protenco.nadea13@gmail.com>
2024-07-08 00:05:20 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+  <div class="carduri-produse">
+  <product-card
+    v-for="product in products"
+    :key="product.id"
+    :title="product.title"
+    :price="product.price"
+    :img="product.img"/>
+</div>
+</template>
+
+<script>
+import ProductCard from "./components/ProductCardComponent.vue"
+export default {
+  components: {
+    ProductCard, },
+
+  data() {
+    return {
+      products: [
+        {id: 0, 
+        title:'Mască regeneratoare',
+        price: '200',
+        img: 'https://i.makeup.md/6/6t/6tfyn3bdegkr.jpg' },
+
+        {id: 1, 
+        title:'Mască hidratantă', 
+        price: '200',
+        img: 'https://licilasicdn.s3.amazonaws.com/public/product_images/39496/main/original.jpg'},
+
+        {id: 2, 
+        title:'Mască păr uscat',
+        price: '300',
+        img: 'https://i.makeup.md/c/c5/c5s7jacaezx0.jpg'},
+
+        {id: 3, 
+        title:'Mască păr blond', 
+        price: '300',
+        img: 'https://m.media-amazon.com/images/I/71NDyeo3vBL._AC_UF1000,1000_QL80_.jpg'}
+      ]
+    }
+  },
+}
+</script>
+
+
+<style lang="css">
+.carduri-produse{
+  display: flex;
+  justify-content: center;
+  gap: 20px; }
+  
+</style>
---
+++ src/components/ProductCardComponent.vue
@@ -0,0 +1,67 @@
+<template lang="html">
+    <div class="card-component">
+        <img class="card-img" :src="img" :alt="title">
+        <h2> {{title}} </h2>
+        <button class="gramaj" @click="selectOption('200ml')">200ml</button>
+        <button class="gramaj" @click="selectOption('400ml')">400ml</button> 
+        <h2>{{ computedPrice }} Lei</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+props: {
+    title: String,
+    price: Number,
+    img: String },
+   
+data() {
+    return {
+      selectedOption: '200ml', 
+    };
+  },
+  methods: {
+    selectOption(option) {
+      this.selectedOption = option;
+    },
+  },
+  computed: {
+    computedPrice() {
+      if (this.selectedOption === '200ml') {
+        return this.price;
+      } else if (this.selectedOption === '400ml') {
+        return this.price * 2; 
+      }
+      return this.price; 
+    },
+  },
+};
+</script>
+
+
+<style lang="css">
+
+.card-component {
+    width: 300px;
+    height: 400px;
+    border: 3px ridge grey;
+    border-radius: 4%;
+    text-align: center;
+    box-sizing: border-box;
+    margin-top: 30px;
+    transition: all 0.2s ease-in-out;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);}
+    .card-component:hover {
+        transform:scale(1.03);}
+    .card-img {
+        width: 150px;
+        height: 150px;
+        margin-top: 40px; }
+    .gramaj {
+        width:100px;
+        height: 50px;
+        margin-left: 10px; }  
+</style>
+
+   
